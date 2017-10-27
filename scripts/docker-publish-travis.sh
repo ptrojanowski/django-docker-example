93d1d9af47001516b0215455e95a688cd1155d9e
Paweł Trojanowski <trojanowski.dev@gmail.com>
2017-10-27 16:49:54 +0200

    fixed typo in docker-publish-travis.sh

--- scripts/docker-publish-travis.sh
+++ scripts/docker-publish-travis.sh
@@ -2,6 +2,6 @@ docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 docker build -f ./compose/production/django/Dockerfile . -t django-docker-example:$DOCKER_TAG --no-cache
 
-docker tag fibon-api:$DOCKER_TAG $DOCKER_USERNAME/django-docker-example:$DOCKER_TAG
+docker tag django-docker-example:$DOCKER_TAG $DOCKER_USERNAME/django-docker-example:$DOCKER_TAG
 
 docker push $DOCKER_USERNAME/django-docker-example:$DOCKER_TAG
